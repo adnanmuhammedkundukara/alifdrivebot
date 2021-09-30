beb22970a9557f6a642933d801afae263e416620
ADNAN <73103641+adnanmuhammedkundukara@users.noreply.github.com>
2021-09-30 10:30:18 +0530

    Create __init__.py

---
+++ alifdrivebot/__init__.py
@@ -0,0 +1,27 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+  Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+Basic Commads
+/start :- check am Alive
+Bot Owner Only
+/search Ebook Free Download 
+"""
+
+ABOUT = """
+➥ Name :  **eBooks Drive bot**
+    
+➥ Creator : [Muhammed Adnan](t.me/mr_readers)
+➥ Language : Python3
+➥ Library : Pyrogram Asyncio 1.13.0
+➥ Source Code : [Click Me](https://github.com/adnanmuhammedkundukara/alifdrivebot)
+"""
