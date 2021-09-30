8919ecacacf68820db7f93918aaddb35c779ea0b
ADNAN <73103641+adnanmuhammedkundukara@users.noreply.github.com>
2021-09-30 11:19:52 +0530

    Update alifdrive.py

--- alifdrive.py
+++ alifdrive.py
@@ -7,7 +7,7 @@ logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from ALIFDRIVEBOT import Media
+from alifdrivebot import Media
 from Config import SESSION, API_ID, API_HASH, BOT_TOKEN
 import pyromod.listen
 
